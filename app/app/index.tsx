5b403c1c106249aff7a624ae6601cee0f486d613
Aneesh <aneesh.hegde@icloud.com>
2025-09-06 21:20:25 +0530

    feat: Implement authentication flow and create login and home screens

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import { supabase } from "@/lib/supabase";
+import { useRouter } from "expo-router";
+import { useEffect } from "react";
+import { View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      if (session) {
+        router.replace("/home");
+      } else {
+        router.replace("/login");
+      }
+    });
+    supabase.auth.onAuthStateChange((_event, session) => {
+      // Doesn't automatically push to /home when signed in so that any other API calls can finish.
+      // Make sure to push to /home after sign in manually.
+      if (!session) {
+        router.replace("/login");
+      }
+    });
+  }, []);
+
+  return <View></View>;
 }
